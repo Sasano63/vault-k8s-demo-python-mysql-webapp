c140fa89a9e67073f015fb4fe795fdb833114fed
Sasano63 <sasano@hashicorp.com>
2022-03-31 14:45:10 +0200

    gke variables

--- 00_1_variables.sh
+++ 00_1_variables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export GCP_PROJECT="hc-a734ff0cc2b24adf8bcaea4e2fc"
+export GCP_PROJECT=""
 export GCP_COMPUTE_ZONE="europe-central2-a"
 export GCP_GKE_NAME="vault-demo-gke-dc01"
 export GCP_GKE_NUM_NODES=3
